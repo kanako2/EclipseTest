1f7b66176fab45cef54e1dc8c682f8300df7f931
kanako2 <ks.k2g60@gmail.com>
2019-10-26 23:24:15 +0900

    Eclipseワークスペースからのコミットテスト

---
+++ 1026/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Map<String,Integer>  prefs = new HashMap<String,Integer>();
+		prefs.put("Kyoto",255);
+		prefs.put("Tokyo",332);
+
+		int tokyo = prefs.get("Tokyo");
+		System.out.println(tokyo);
+
+		for (String key :prefs.keySet()) {
+			int value = prefs.get(key);
+			System.out.println(value + key);
+		}
+
+	}
+
+
+
+}
